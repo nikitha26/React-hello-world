3fa55c8d8730b869842656b01ced20b19053a798
nikitha26 <mnikitha365@gmail.com>
2020-09-26 20:44:50 +0530

    My First React App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import "./style.css";
+import Button from './Button';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    return (
+        <div>
+            <h1>Hello world</h1>
+            <Button title="App Store"/>
+            <Button title="Sub mit"/>
+            <Button title="Sub mit"/>
+            <Button title="Sub mit"/>
 
+        </div>   
+    )
+}
 export default App;
+
---
+++ src/Button.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const btn = ({title}) => (
+    <div>
+       <button className="button1">{title}</button>
+    </div>    
+)
+
+export default btn;
