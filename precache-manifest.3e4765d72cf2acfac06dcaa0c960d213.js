96be13a1e6c8f5d7a4a1590ae74a253b6b6ee3f0
nikitha26 <mnikitha365@gmail.com>
2020-09-26 20:46:41 +0530

    Updates

---
+++ precache-manifest.3e4765d72cf2acfac06dcaa0c960d213.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53cddf92ef098a693aee1b6a2b9134b9",
+    "url": "/React-hello-world/index.html"
+  },
+  {
+    "revision": "02edd4d3b232fd2b0c3a",
+    "url": "/React-hello-world/static/css/main.6a1d5e1c.chunk.css"
+  },
+  {
+    "revision": "d8a3c0ab8584ce5e94f3",
+    "url": "/React-hello-world/static/js/2.b6f6008c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-hello-world/static/js/2.b6f6008c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02edd4d3b232fd2b0c3a",
+    "url": "/React-hello-world/static/js/main.c4a802fc.chunk.js"
+  },
+  {
+    "revision": "1f023162398ca877b721",
+    "url": "/React-hello-world/static/js/runtime-main.7a8d96d5.js"
+  }
+]);
