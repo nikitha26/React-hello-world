02a8ac1ff8282164e047de0c986a28746d1879e2
nikitha26 <mnikitha365@gmail.com>
2020-10-31 12:28:31 +0530

    hello world

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import "./style.css";
-import Button from './Button';
 function App() {
     return (
         <div>
-            <h1>Hello world</h1>
-            <Button title="App Store"/>
-            <Button title="Sub mit"/>
-            <Button title="Sub mit"/>
-            <Button title="Sub mit"/>
-
+            <h1>Hello world!!!!</h1>
         </div>   
     )
 }
--- src/Button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const btn = ({title}) => (
-    <div>
-       <button className="button1">{title}</button>
-    </div>    
-)
-
-export default btn;
